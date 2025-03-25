29dfd3fbc7107ee2de69bb218c235759c240835e
mn 81 <mnizamudheen81@gmail.com>
2025-03-26 01:03:58 +0530

    fixed bugs in wallet rendering

--- controller/user/walletController.js
+++ controller/user/walletController.js
@@ -1,4 +1,4 @@
-const user = require("../../model/usersSchema");
+const User = require("../../model/usersSchema");
 const variant = require("../../model/varient");
 const products = require("../../model/productSchema");
 const walletModel = require("../../model/walletSchema");
@@ -10,11 +10,12 @@ const loadWallet = async (req, res) => {
     const walletData = await walletModel.findOne({ userId: user });
     const userid = req.session.user;
 
-    const users = await user.findOne({ _id: userid });
+    const users = await User.findOne({ _id: userid });
+
     const firstLetter = users.FirstName.charAt(0);
     res.render("user/wallet", { walletData, users, firstLetter });
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
     console.log("error in loadcontroller");
   }
 };
